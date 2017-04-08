7a34f3ba96580518bcccb6b7f10b5a9fe3c48e18
debkbanerji <debkbanerji@gmail.com>
2017-04-08 19:45:46 +0000

    Added get out logic

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -26,17 +26,33 @@ showerRef = db.child("showers").child(this_shower_id)
 occupiedRef = showerRef.child("is_occupied")
 
 GPIO.setmode(GPIO.BOARD)         #Set GPIO pin numbering
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Enable input and pull up resistors
-
+GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Enable input and pull up resistors
+GPIO.setup(12,GPIO.OUT)
+GPIO.output(12,GPIO.LOW)
 
 is_occupied = False
+get_out = False
 
 while True:
-    button_input_state = GPIO.input(12) #Read and store value of input to a variable
+    button_input_state = GPIO.input(32) #Read and store value of input to a variable
     is_pressed = not button_input_state
     if is_pressed != is_occupied:
         is_occupied = is_pressed
-        db.child("showers").child(this_shower_id).child("is_occupied").set(is_occupied)
-        print('State Switched')   #Print 'Button Pressed'
+        firebase.database().child("showers").child(this_shower_id).child("is_occupied").set(is_occupied)
+        if (not is_occupied):
+            firebase.database().child("showers").child(this_shower_id).child("get_out").set(False)
+        print('Occupied State Switched')
         print(is_occupied)
         time.sleep(0.3)           #Delay of 0.3s
+    get_out_result = firebase.database().child("showers").child(this_shower_id).child("get_out").get()
+    if get_out_result.val() != get_out:
+        get_out = get_out_result.val()
+        print('Get Out State Switched')
+        print(get_out)
+        if get_out:
+            GPIO.output(12,GPIO.HIGH)
+        else:
+            GPIO.output(12,GPIO.LOW)
+
+
+        
