1a91d50b9b0534dd39632a1e38fc546f75d98bf2
debkbanerji <debkbanerji@gmail.com>
2017-04-08 22:10:08 +0000

    Added more music samples

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -28,7 +28,8 @@ showerRef = db.child("showers").child(this_shower_id)
 occupiedRef = showerRef.child("is_occupied")
 
 GPIO.setmode(GPIO.BOARD)         #Set GPIO pin numbering
-GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Enable input and pull up resistors
+SENSOR_PIN = 11 #32 for button, or 11 for water sensor
+GPIO.setup(SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Enable input and pull up resistors
 GPIO.setup(12,GPIO.OUT)
 GPIO.output(12,GPIO.LOW)
 
@@ -39,16 +40,22 @@ song_path = "Shake\ It\ Off.mp3"
 is_playing = False
 
 
-def play_music():
+def play_music(target_song_path):
     volume = -1000
-    full_path = os.path.join(script_dir, "music_samples/" + song_path)
+    print("SONG PATH")
+    print(target_song_path)
+    full_path = os.path.join(script_dir, "music_samples/" + target_song_path)
+    full_path = full_path.replace(" ", "\\ ")
+    print(full_path)
     command = "omxplayer --vol " + str(volume) + " " + full_path
     # print(command)
+    is_playing = True
+    song_path = False
     os.system(command)
 
 
 while True:
-    button_input_state = GPIO.input(32) #Read and store value of input to a variable
+    button_input_state = GPIO.input(SENSOR_PIN) #Read and store value of input to a variable
     is_pressed = not button_input_state
 
     
@@ -72,8 +79,12 @@ while True:
         else:
             GPIO.output(12,GPIO.LOW)
     if not is_playing:
-        thread.start_new_thread(play_music, ())
-        is_playing = True
+        song_path = firebase.database().child("main_song_path").get().val()
+        if song_path:
+            # print(song_path)
+            thread.start_new_thread(play_music, (song_path, ))
+            firebase.database().child("main_song_path").set(False)
+
 
 
     
