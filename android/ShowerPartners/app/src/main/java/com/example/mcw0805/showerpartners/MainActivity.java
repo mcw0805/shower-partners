63683f7d8d526d272253edfdd2f697ee84a5b608
Chaewon Min <mcw0805@gmail.com>
2017-04-08 10:20:15 -0400

    Created Check Showers Page

--- android/ShowerPartners/app/src/main/java/com/example/mcw0805/showerpartners/MainActivity.java
+++ android/ShowerPartners/app/src/main/java/com/example/mcw0805/showerpartners/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.mcw0805.showerpartners;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference mRootRef;
-    private DatabaseReference mShowersRef;
+//    private DatabaseReference mRootRef;
+//    private DatabaseReference mShowersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRootRef = FirebaseDatabase.getInstance().getReference();
-        mShowersRef = mRootRef.child("showers");
+//        mRootRef = FirebaseDatabase.getInstance().getReference();
+//        mShowersRef = mRootRef.child("showers");
 
-        mShowersRef.setValue("TEST");
+        Button checkShowersButton = (Button) findViewById(R.id.checkShowersButton);
+        checkShowersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent checkShowersIntent = new Intent(MainActivity.this, CheckShowersActivity.class);
+                startActivity(checkShowersIntent);
+                finish();
+            }
+        });
     }
 }
